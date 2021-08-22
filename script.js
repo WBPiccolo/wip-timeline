8064f139dc31d3b41b72baa4caaf564b891a43dd
francesco.piccolo <francesco.piccolo@lynxspa.com>
2021-08-22 19:09:29 +0200

    logica visualizzazione, ordinamento e filtraggio

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+//import {PlanetElement} from './planetElement.js'
+let planets = [];
 init();
 
 async function init() {
     const plantesURL = 'https://swapi.dev/api/planets';
-    getPlanets(plantesURL).then((planets) => {
-        popolaTimeline(planets)
+    getPlanets(plantesURL).then((resPlanets) => {
+        planets = resPlanets;
+        popolaTimeline(resPlanets)
     });
 }
 
@@ -22,10 +25,54 @@ async function getPlanets(apiURL) {
 }
 
 function popolaTimeline(planets){
-    console.log(planets);
-    let planetList = document.getElementById('listapianeti');
-    planets.forEach(pianeta => {
-        planetList.innerHTML += pianeta.name
+    const timeline = document.getElementById('timeline');
+    timeline.innerHTML='';
+    planets.forEach((pianeta, index) => {
+        const direction = index%2? 'right' : 'left';
+        const planetContainer = document.createElement('div');
+        planetContainer.classList.add('container',direction);
+        const planetContent = document.createElement('div');
+        planetContent.classList.add('content');
+        const planetLine = document.createElement('h2');
+        planetLine.innerText = pianeta.name;
+        const planetDate = document.createElement('p');
+        planetDate.innerText = pianeta.created;
+
+        planetContent.appendChild(planetLine);
+        planetContent.appendChild(planetDate);
+        planetContainer.appendChild(planetContent);
+        timeline.appendChild(planetContainer);
+        
     });
+}
+
+function invertPlanets(){
+    popolaTimeline(planets.reverse());
+}
+
+function searchPlanets(){
+    const startValue = document.getElementById('start').value;
+    const endValue = document.getElementById('end').value;
+    const startDate = new Date(startValue);
+    const endDate = new Date(endValue);
+    startDate.setHours(23,59,59,999);
+    endDate.setHours(23,59,59,999);
+
+    if(isNaN(startDate.getTime())){
+        console.log('startDate ko');
+        return;
+    }
+
+    if(isNaN(endDate.getTime())){
+        console.log('endDate ko');
+        return;
+    }
+
+    const filteredPlanets = planets.filter((planet)=> {
+        const creationDate = new Date(planet.created);
+        return (creationDate.getTime()>= startDate.getTime())&&(creationDate.getTime()<= endDate.getTime());
+    });
+
+    popolaTimeline(filteredPlanets);
     
 }
